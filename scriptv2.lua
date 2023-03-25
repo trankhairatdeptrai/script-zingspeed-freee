b86027370beb7d1a17ddbee628b9999123f4c593
trankhairatdeptrai <128835990+trankhairatdeptrai@users.noreply.github.com>
2023-03-25 21:17:20 +0700

    Create scriptv2.lua

---
+++ scriptv2.lua
@@ -0,0 +1,249 @@
+gg.alert("Subscribe My Youtube Channel   =====================================   	YT: F4X GAMING")
+function IAM()
+MN = gg.choice({
+"💠ZS💠",
+" ᴇxɪᴛ "}, nil, (os.date("F4X GAMING             =================================================   ᴛᴏᴅᴀʏ: 📅 %Y/%m/%d           ᴛɪᴍe ⏱️: %H:%M:%S")))
+if MN == nil then else
+if MN == 1 then LUA() end
+if MN == 2 then EXIT() end
+end
+f4x = -1
+end
+function LUA()
+MN1 = gg.multiChoice({
+"💀 One Click Zs [GAME]",
+"Nâng Cấp Động Cơ [LOBBY]",
+"Gia Tốc 1.3 [LOBBY]",
+"Auto Win Part 1 [LOBBY]",
+"Auto Win Part 2 [GAME]",
+"🔙ʙᴀᴄᴋ"}, nil, "🛠F4X GAMING🛠")
+if MN == nil then else
+if MN1[1] == true then OneClick1() end
+if MN1[2] == true then wall() end
+if MN1[3] == true then wallhack() end
+if MN1[4] == true then antiban() end
+if MN1[5] == true then antiban1() end
+if MN1[6] == true then IAM() end
+end
+f4x = -1
+end
+function OneClick1() 
+while true do
+if gg.isVisible() then
+		gg.setVisible(false)
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("65537D;22339703D;45F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("45", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("300", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.searchNumber("35", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("35", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.searchNumber("1;1000:5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1.5", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.clearResults()
+gg.searchNumber("0.1;45;65;50;70;300", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("50", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("250", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.searchNumber("1.0F;85.0F;105.0F;155.0F;0.89999997616F;1.0F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0x752f000000, 0x752f09ffffff, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("2.2", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Xong")
+gg.toast("Khải Đẹp Troai")
+end
+end
+end
+
+function wall()
+  mw6sds6 = gg.multiChoice({
+    "ON [Lobby]",
+    "OFF [Lobby]",
+    "⎋BACK"
+  }, nil, "Subscribe My Youtube Channel   =====================================   	YT: F4X GAMING")
+  if mw6sds6 == nil then
+  else
+    if mw6sds6[1] == true then
+      mwhs1()
+    end
+    if mw6sds6[2] == true then
+      mwhs2()
+    end
+    if mw6sds6[3] == true then
+      MENUWEAPON153()
+    end
+  end
+  PUBGMH = -1
+end
+function mwhs1()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.clearResults()
+gg.searchNumber("0.27777779102", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("0.27777779102", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("0.37777779102", gg.TYPE_FLOAT)
+gg.clearResults()
+
+end
+function mwhs2()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.clearResults()
+gg.searchNumber("0.37777779102", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("0.37777779102", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("0.27777779102", gg.TYPE_FLOAT)
+gg.clearResults()
+
+end
+function wallhack()
+mw6sds60 = gg.multiChoice({
+    "1.1 [Lobby]",
+    "1.2 [Lobby]",
+    "1.3 [Lobby]",
+    "1.4 [Lobby]",
+    "1.5 [Lobby]",
+    "⎋BACK"
+  }, nil, "Subscribe My Youtube Channel   =====================================   	YT: F4X GAMING")
+  if mw6sds6 == nil then
+  else
+    if mw6sds60[1] == true then
+      mwhs12()
+    end
+    if mw6sds60[2] == true then
+      mwhs23()
+    end
+    if mw6sds60[3] == true then
+      mwhs231()
+    end
+    if mw6sds60[4] == true then
+      mwhs232()
+    end
+    if mw6sds60[5] == true then
+      mwhs233()
+    end
+    if mw6sds60[6] == true then
+      MENUWEAPON153()
+    end
+  end
+  PUBGMH = -1
+end
+function mwhs12()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.clearResults()
+gg.searchNumber("1.00390625F;4.24794661E-39F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1.00390625F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1.1", gg.TYPE_FLOAT)
+gg.clearResults()
+
+end
+function mwhs23()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.clearResults()
+gg.searchNumber("1.00390625F;4.24794661E-39F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1.00390625F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1.2", gg.TYPE_FLOAT)
+gg.clearResults()
+end
+function mwhs231()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.clearResults()
+gg.searchNumber("1.00390625F;4.24794661E-39F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1.00390625F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1.3", gg.TYPE_FLOAT)
+gg.clearResults()
+
+end
+function mwhs232()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.clearResults()
+gg.searchNumber("1.00390625F;4.24794661E-39F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1.00390625F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1.4", gg.TYPE_FLOAT)
+gg.clearResults()
+
+end
+function mwhs233()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.clearResults()
+gg.searchNumber("1.00390625F;4.24794661E-39F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("1.00390625F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1.5", gg.TYPE_FLOAT)
+gg.clearResults()
+
+end
+function antiban()
+  mw6sds61 = gg.multiChoice({
+    "ON [Lobby]",
+    "OFF [Lobby]",
+    "⎋BACK"
+  }, nil, "Subscribe My Youtube Channel   =====================================   	YT: F4X GAMING")
+  if mw6sds61 == nil then
+  else
+    if mw6sds61[1] == true then
+      mwhs10()
+    end
+    if mw6sds61[2] == true then
+      mwhs20()
+    end
+  end
+  PUBGMH = -1
+end
+function mwhs10()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.clearResults()
+gg.searchNumber("0.27777779102", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("0.27777779102", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("99.27777779102", gg.TYPE_FLOAT)
+gg.clearResults()
+
+end
+function mwhs20()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.clearResults()
+gg.searchNumber("99.27777779102", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("99.27777779102", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("0.27777779102", gg.TYPE_FLOAT)
+gg.clearResults()
+
+end
+function antiban1()
+gg.clearResults()
+gg.searchNumber("1.0F;85.0F;105.0F;155.0F;0.89999997616F;1.0F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0x752f000000, 0x752f09ffffff, 0)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+revert = gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("99", gg.TYPE_FLOAT)
+gg.clearResults()
+
+end
+function EXIT()
+gg.alert("Follow me YT: F4X GAMING ")
+gg.skipRestoreState()
+gg.setVisible(true)
+os.exit()
+end
+while true do
+if gg.isVisible(true) then
+f4x = 1
+gg.setVisible(false) end
+if f4x == 1 then 
+IAM()
+end
+end
